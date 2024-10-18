aa6b03a568c1091cd3ad01b518348ba016a53c3f
arista <arista@abramsons.info>
2024-10-18 01:37:52 +0000

    update to test

--- html/test.js
+++ html/test.js
@@ -2,6 +2,8 @@ function bodyLoaded() {
   Dustel.renderInto(document.getElementById("body"),
                     ["div", {class: "bigfont"}, [
                       ctx=>googleLink(ctx),
+                      ctx=>delayed(ctx, 1000, "1000ms!"),
+                      ctx=>delayed(ctx, 1500, "1500ms!"),
                       "Hi there!",
                     ]]
                    )
@@ -22,5 +24,19 @@ function googleLink(ctx) {
     "no",
     ["button", {on: {click}}, [`Click Me!: ${ctx.state}`]],
     ["span", ["yipe"]],
+  ]]
+}
+
+function delayed(ctx, msec, str) {
+  if (ctx.initializeState(null)) {
+    (async ()=>{
+      await new Promise(resolve=>setTimeout(resolve, msec))
+      ctx.state = true
+      ctx.update()
+    })()
+  }
+
+  return ["div", [
+    ctx.state ? `Waited ${msec} ms for ${str}` : null
   ]]
 }
